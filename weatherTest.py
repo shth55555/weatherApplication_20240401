27df02353910198e3f9247dbbe0cc66ac146016d
shth55555 <shth5555@naver.com>
2024-04-01 15:46:48 +0900

    날씨지역정보, 현재온도 텍스트 크롤링

--- weatherTest.py
+++ weatherTest.py
@@ -20,3 +20,26 @@ todayTempText = todayTempText[6:12].strip()  # 6번째 글자부터 슬라이싱
 print(todayTempText)
 
 
+yesterdayTempText = weatherSoup.find("span", "temperature up").text
+yesterdayTempText = yesterdayTempText.strip()
+print(yesterdayTempText)
+
+todayTempText = weatherSoup.find("span", {"class":"weather before_slash"}).text
+todayTempText = todayTempText.strip()
+print(todayTempText)
+
+
+senseTempText = weatherSoup.find("dd", {"class":"desc"}).text #현재 체감온도
+senseTempText = senseTempText.strip()
+print(senseTempText)
+
+todayInfoText = weatherSoup.select("ul.today_chart_list>li")  #미세먼지, 초미세먼지, 자외선, 일몰
+# print(todayInfoText)
+# print("-----------------------------")
+# print(todayInfoText[0])
+dustInfo = todayInfoText[0].find("span", {"class":"txt"}).text
+dustInfo = dustInfo.strip()
+print(dustInfo)   #미세먼지 정보
+
+dustInfo2 = todayInfoText[1].find("span",{"class":"txt"}).text
+print(dustInfo2)    #초미세먼지 정보
